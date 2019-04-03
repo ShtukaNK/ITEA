9b01ac70faa64f489b9db933243fa8ed9cdb8577
ShtukaNK <diabolisht@gmail.com>
2019-04-03 19:52:59 +0300

    itea

--- ConsoleApp/ConsoleApp21/Fibonacci.cs
+++ ConsoleApp/ConsoleApp21/Fibonacci.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp21
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Fibonacci(10));
         }
+        static int Fibonacci(int x)
+        {
+            if (x <= 1)
+                return 1;
+            return Fibonacci(x - 1) + Fibonacci(x - 2);
+        }
+
+        
+    }
     }
 }
